517f49664680e5693551657b6db4ff93d24462a0
Zavartsev Nikita <shaxx96@mail.ru>
2021-06-16 15:00:19 +0300

    first point

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,7 +2,30 @@ from django import forms
 
 from .models import Product
 
+
 class ProductForm(forms.ModelForm):
+
+    title = forms.CharField(
+        label='', widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your Title"
+            }
+        )
+    )
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class-name two",
+                "id": "my-id-for-textarea",
+                "rows": 20,
+                "cols": 120
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -10,3 +33,34 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+
+        if not "CFE" in title:
+            raise forms.ValidationError("This is not a valid title")
+        return title
+
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(
+        label='', widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your Title"
+            }
+        )
+    )
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "class": "new-class-name two",
+                "id": "my-id-for-textarea",
+                "rows": 20,
+                "cols": 120
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 from .models import Product
+
+
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+            my_form = RawProductForm()
+        else:
+            print(my_form.errors)
 
     context = {
-        'form': form
+        'form': my_form
     }
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#         # Product.objects.create(title=my_new_title)
+#
+#     context = {}
+#     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     initial_data = {
+#         "title": "My Awesome Title"
+#     }
+#     obj = Product.objects.get(id=1)
+#     form = ProductForm(request.POST or None, instance=obj)
+#     if form.is_valid():
+#         form.save()
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
+
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
@@ -22,3 +57,32 @@ def product_detail_view(request):
         'object': obj
     }
     return render(request, "products/product_detail.html", context)
+
+
+def dynamic_lookup_view(request, id):
+    # obj = Product.objects.get(id=my_id)
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        'object': obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        'object': obj
+    }
+    return render(request, "products/product_delete.html", context)
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
